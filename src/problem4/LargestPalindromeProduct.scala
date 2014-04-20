ec8b121048e0ce7913dd6c3d2d3e9c01b1802bb1
hkw2028 <hkw2028@gmail.com>
2014-04-20 23:09:33 +0900

    solution to problem4

---
+++ src/problem4/LargestPalindromeProduct.scala
@@ -0,0 +1,30 @@
+package problem4
+import scala.util.control.Breaks._
+object LargestPalindromeProduct {
+	def main(args: Array[String]) {
+	  var largestProduct = -1
+	  for(i <- 100 to 1000 ){
+	    for( j <- 100 to 1000 ){
+	      var product = i * j
+	      var prodInStr = product.toString
+	      var prodLastindex = prodInStr.length - 1
+	      var isPalindromic = true
+	      println(prodInStr)
+	      for( k <- 0 to (prodInStr.length/2)){	    	  
+	    	  println("prodInStr.charAt("+k+") : "+prodInStr.charAt(k))
+	    	  println("prodInStr.charAt(prodLastindex-"+k+") : "+prodInStr.charAt(prodLastindex-k))
+	    	  isPalindromic &= (prodInStr.charAt(k) == prodInStr.charAt(prodLastindex-k))
+	      }
+	      if(isPalindromic){
+	        println("Palindromic product : "+product)
+	        if(largestProduct < product){
+	          largestProduct = product
+	        }
+	      }
+	    }
+	  }
+	  
+	  println(" largest Palindromic product : "+largestProduct)
+	  
+	}
+}
